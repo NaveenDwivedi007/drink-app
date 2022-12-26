019d1fbb65b282bc96fc55f8dcaaaeb59d0ad3f7
Naveen Dwivedi <naveen.kumar2@pristyncare.com>
2022-12-26 22:36:19 +0530

    page not found  added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
-    path:'',
+    path:'drinks',
     loadChildren:() => import('./modules/drinks/drinks.module').then(m => m.DrinksModule), 
   },
+  {
+    path:"**",
+    component:PageNotFoundComponent
+  }
 ];
 
 @NgModule({
